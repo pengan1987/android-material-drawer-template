46fbc6c5c198b7892cedff62db16781e3472f32e
Pengan Zhou <pengan1987@msn.com>
2015-03-14 00:09:46 -0700

    a little demo for switch between different activities

--- app/src/main/java/com/poliveira/apps/materialtests/SecondActivity.java
+++ app/src/main/java/com/poliveira/apps/materialtests/SecondActivity.java
@@ -1,46 +1,55 @@
 package com.poliveira.apps.materialtests;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.RecyclerView;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import java.util.List;
 
-
-public class MainActivity extends ActionBarActivity implements NavigationDrawerCallbacks {
-
+public class SecondActivity extends ActionBarActivity implements NavigationDrawerCallbacks{
     private Toolbar mToolbar;
     private NavigationDrawerFragment mNavigationDrawerFragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
+
         mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
+        getSupportActionBar().setTitle("Second Activity");
 
         mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.fragment_drawer);
 
         mNavigationDrawerFragment.setup(R.id.fragment_drawer, (DrawerLayout) findViewById(R.id.drawer), mToolbar);
-        mNavigationDrawerFragment.bindMenuData(0);
-
-
+        mNavigationDrawerFragment.bindMenuData(1);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         Intent intent;
@@ -61,13 +70,5 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
             default:
                 break;
         }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (mNavigationDrawerFragment.isDrawerOpen())
-            mNavigationDrawerFragment.closeDrawer();
-        else
-            super.onBackPressed();
     }
 }
